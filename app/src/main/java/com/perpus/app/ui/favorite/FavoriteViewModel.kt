a2c22278bddb827f354a6fc9db740841045158e6
mbareb-leonardo14 <mbareb.le@students.amikom.ac.id>
2020-12-30 09:42:01 +0700

    Tugas PCS

---
+++ app/src/main/java/com/perpus/app/ui/favorite/FavoriteViewModel.kt
@@ -0,0 +1,7 @@
+package com.perpus.app.ui.favorite
+
+import androidx.lifecycle.ViewModel
+
+class FavoriteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/perpus/app/ui/home/MainActivity.kt
+++ app/src/main/java/com/perpus/app/ui/home/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.perpus.app.R
 import com.perpus.app.databinding.ActivityMainBinding
 import com.perpus.app.ui.auth.AuthActivity
@@ -11,14 +12,27 @@ import com.perpus.app.ui.auth.PerpusAuth
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.buttonLogout.setOnClickListener {
-            PerpusAuth.logout(this) {
-                startActivity(Intent(this, AuthActivity::class.java))
-                finish()
+        init()
+    }
+
+    private fun init() {
+        binding.buttomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.ic_home -> navigate(R.id.homeFragment)
+                R.id.ic_news -> navigate(R.id.newsFragment)
+                R.id.ic_favorite -> navigate(R.id.favoriteFragment)
+                R.id.ic_profile -> navigate(R.id.profileFragment)
+                else -> false
             }
         }
+    }
+
+    private fun navigate(id: Int): Boolean {
+        findNavController(R.id.navHost).navigate(id)
+        return true
     }
 }
